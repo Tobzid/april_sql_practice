359a1f83ee32f5e422da9715c3b84b2328f4a3b3
Tobzid <tobzid2013@gmail.com>
2026-06-04 13:50:01 -0700

    creating table in SQL

--- first.sql
+++ first.sql
@@ -1,1 +1,9 @@
-#This is a practice SQL project
+ #This is a practice SQL project
+
+CREATE TABLE EmployeeDemographics
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)

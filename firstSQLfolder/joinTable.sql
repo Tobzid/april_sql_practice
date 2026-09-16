1b68e4e49ededc0a98637fc20283b923b484ba26
Tobzid <tobzid2013@gmail.com>
2026-09-16 00:26:08 -0700

    Joining tables in sql

---
+++ firstSQLfolder/joinTable.sql
@@ -0,0 +1,7 @@
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+from PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+	where dea.continent is not null
+	order by 2,3
